e5a38fca4750b4f5217cd99ce23591a64242401c
Jeffrey Ellin <jeff@ellin.com>
2014-05-28 22:31:51 -0400

    env script

---
+++ gemfire7/env.sh
@@ -0,0 +1,15 @@
+# Get script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export GEMFIRE_VERSION=7
+
+export GEMFIRE=/Users/jellin/Downloads/Pivotal_GemFire_702_b45797
+export GFE_CONFIG_DIR=$DIR/../conf
+export GFE_LOG_DIR=$DIR/../log
+export DEFAULT_LOCATOR_HOST=localhost
+export DEFAULT_LOCATOR_PORT=41111
+export DEFAULT_CLUSTER=NearRealTimeRisk
+export DEFAULT_NODES=2
+export GFE_APP_JAR_DIR=$DIR/../lib
+
+export PATH=$PATH:$GEMFIRE/bin
